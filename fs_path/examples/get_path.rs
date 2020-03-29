84b5c3af2bdc04970cf86e53408735482cc32ba0
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-03-29 11:00:39 +0200

    Modified example for path

---
+++ fs_path/examples/get_path.rs
@@ -0,0 +1,15 @@
+//! Retrieves the path of a file from an `std::fs::File` handle.
+
+use std::fs;
+use std::ffi::OsStr;
+use fs_path::FilePath;
+
+fn main() -> std::io::Result<()> {
+    let f = fs::File::create("test.txt")?;
+    let path = f.path()?;
+    assert!(path.exists());
+    assert_eq!(path.file_name(), Some(OsStr::new("test.txt")));
+    println!("Full path: {:?}", path);
+    fs::remove_file("test.txt")?;
+    Ok(())
+}
--- fs_path/examples/test.rs
+++
@@ -1,9 +0,0 @@
-
-use std::fs::File;
-use fs_path::*;
-
-fn main() -> std::io::Result<()> {
-    let f = File::open("C:/TMP/briefing.xml")?;
-    println!("Path: {:?}", f.path()?);
-    Ok(())
-}
