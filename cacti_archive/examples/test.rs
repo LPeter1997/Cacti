db1a70954ce803feec7571cbc96a82bd4ccf7e98
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-04-10 17:53:24 +0200

    Working zip API

--- cacti_archive/examples/test.rs
+++ cacti_archive/examples/test.rs
@@ -1,6 +1,17 @@
 
-use cacti_zip::*;
+use std::fs;
+use std::io::Read;
+use cacti_archive::*;
 
 fn main() {
-
+    let f = fs::File::open("C:/TMP/testzippy.ZIP").expect("Could not open file!");
+    let mut archive = zip::ZipArchive::parse(f).expect("Could not parse zip!");
+    let mut file = archive.entry_at_index(3).expect("Could not get entry!");
+    println!("Fname: {}", file.name());
+    let mut dec = file.decompressor().expect("Can't decompress file!");
+    let mut content = Vec::new();
+    dec.read_to_end(&mut content).expect("Can't decompress 2!");
+    for c in content {
+        print!("{}", c as char);
+    }
 }
--- cacti_archive/src/lib.rs
+++ cacti_archive/src/lib.rs
@@ -1,8 +1,5 @@
 //! Cross-platform and dependency-free archive handling.
 // TODO: doc, introduce library
 
-mod deflate;
-pub use deflate::Inflate;
-
-mod zip;
-//pub use zip::ZipArchive;
+pub mod deflate;
+pub mod zip;
