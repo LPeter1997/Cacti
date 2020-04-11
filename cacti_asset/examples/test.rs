b32832db1d766a6b073e535f0df850d7ef406858
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-04-11 10:38:52 +0200

    Simplified example

--- cacti_asset/examples/test.rs
+++ cacti_asset/examples/test.rs
@@ -3,8 +3,7 @@ use cacti_asset::dyn_lib::*;
 
 fn main() {
     let mut lib = Library::load("kernel32").expect("Could not load library!");
-    let mut sym: Symbol<extern "system" fn(u32) -> u32> = lib.load_symbol("GetProcessVersion").expect("Could not load symbol!");
-    let f: extern "system" fn(u32) -> u32 = *sym;
-    let v = f(0);
+    let sym: Symbol<extern "system" fn(u32) -> u32> = lib.load_symbol("GetProcessVersion").expect("Could not load symbol!");
+    let v = sym(0);
     println!("Ree: {}", v);
 }
