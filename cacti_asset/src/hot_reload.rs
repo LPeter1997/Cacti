663d423aca909f831bb90640c930dcfbec129a44
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-04-12 20:09:44 +0200

    Redid hot-reload approach

--- cacti_asset/src/hot_reload.rs
+++ cacti_asset/src/hot_reload.rs
@@ -3,69 +3,77 @@
 
 use std::path::{Path, PathBuf};
 use std::io;
+use std::time::SystemTime;
 use crate::dyn_lib::*;
 
+// ////////////////////////////////////////////////////////////////////////// //
+//                                    API                                     //
+// ////////////////////////////////////////////////////////////////////////// //
+
+/// The type responsible for managing the dinamically loaded part of the
+/// application. This type should be held in the part that won't be
+/// live-reloaded.
+pub struct Host {
+    /// Path to the client library.
+    path   : PathBuf,
+    /// The loaded client library.
+    library: Library,
+    /// The last modification time read from the client library's path.
+    mtime  : SystemTime,
+    /// The loaded symbol from the client library.
+    client : ClientApi,
+}
+
+/// This is the trait that the live-reloaded state type should implement. These
+/// are the functions called by the `Host` to update the application.
+pub trait Client {
+    /// The type of host API this client expects. This is received from the host
+    /// as a way to use services provided by it.
+    type HostApi;
+
+    /// Creates a new, initial client state.
+    fn new(api: &mut Self::HostApi) -> Self;
+    /// Migrates the old state - represented as a slice of bytes - to the new,
+    /// current version.
+    fn migrate(old: &mut [u8], api: &mut Self::HostApi) -> Self;
+
+    /// Called before the first `reload`, to initialize things in the state.
+    fn initialize(&mut self, api: &mut Self::HostApi);
+    /// Called every time after the new code is loaded.
+    fn reload(&mut self, api: &mut Self::HostApi);
+    /// Called every time before the old code is unloaded.
+    fn unload(&mut self, api: &mut Self::HostApi);
+    /// Called after the last `unload` to clean up.
+    fn terminate(&mut self, api: &mut Self::HostApi);
+    /// Called to perform update on the application. The returned `Loop` variant
+    /// will determine if the client application should continue running or
+    /// terminate.
+    fn update(&mut self, api: &mut Self::HostApi) -> Loop;
+}
+
+/// A descriptive type to control the main application loop.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Loop {
+    /// The client application should continue running.
     Continue,
+    /// The client application should be terminated.
     Stop,
 }
 
-/// The trait that every live-reloadable code must implement.
-pub trait Client {
-    /// The type of state the client requires to operate on.
-    type State;
+// ////////////////////////////////////////////////////////////////////////// //
+//                               Implementation                               //
+// ////////////////////////////////////////////////////////////////////////// //
 
-    /// Called once, before the first loaded event.
-    fn initialize(state: &mut Self::State);
+#[repr(C)]
+pub struct ClientApi {
+    state_size   : u64,
+    new_state    : fn(*mut u8, *mut u8),
+    migrate_state: fn(*mut u8, usize, *mut u8, *mut u8),
+    drop_state   : fn(*mut u8),
 
-    /// Called when the client's code is loaded into memory.
-    fn reload(state: &mut Self::State);
-
-    /// Called when the client's code is about to be unloaded from memory.
-    fn unload(state: &mut Self::State);
-
-    /// Called when the client is about to be destroyed.
-    fn terminate(state: &mut Self::State);
-
-    /// Called in regular intervals to update the client.
-    fn update(state: &mut Self::State) -> Loop;
-}
-
-pub struct Host<C: Client> {
-    lib_path: PathBuf,
-    // TODO: We box for now because of lifetime issues
-    lib: Box<Library>,
-    state: C::State,
-}
-
-impl <C: Client> Host<C> {
-    pub fn new(lib_path: impl AsRef<Path>) -> io::Result<Self> where C::State: Default {
-        Self::with_state(lib_path, C::State::default())
-    }
-
-    pub fn with_state(lib_path: impl AsRef<Path>, state: C::State) -> io::Result<Self> {
-        let lib_path = lib_path.as_ref();
-        let mut lib = Box::new(Library::load(lib_path)?);
-        // TODO: Symbol name?
-        // TODO: Symbol type?
-        let _sym: Symbol<*mut ()> = lib.load_symbol("")?;
-        // TODO: Load symbol stuff?
-        Ok(Self{
-            lib_path: lib_path.to_path_buf(),
-            lib,
-            state,
-        })
-    }
-
-    pub fn run(&mut self) {
-        // TODO: Init
-        loop {
-            // TODO: Check if needed to reload
-                // TODO: If so, unload, reload
-            // TODO: Call update
-            // TODO: If terminated, break loop
-        }
-        // TODO: Terminate
-    }
+    initialize   : fn(*mut u8, *mut u8),
+    reload       : fn(*mut u8, *mut u8),
+    unload       : fn(*mut u8, *mut u8),
+    terminate    : fn(*mut u8, *mut u8),
+    update       : fn(*mut u8, *mut u8) -> u32,
 }
