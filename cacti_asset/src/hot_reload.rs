e86cb3deebcdf2f743436a95413c0860ac635eae
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-04-12 22:45:19 +0200

    Wrote update function

--- cacti_asset/src/hot_reload.rs
+++ cacti_asset/src/hot_reload.rs
@@ -25,27 +25,85 @@ pub struct Host<API> {
     /// The loaded symbol from the client library.
     client : ClientApi,
     /// The state of the client.
-    state  : Vec<u8>,
+    state  : Option<Vec<u8>>,
     /// The API type the host provides from it's side.
     api    : API,
 }
 
 impl <API> Host<API> {
+    /// Creates a new host from the provided API and path for the client
+    /// library.
     pub fn new(mut api: API, path: impl AsRef<Path>) -> io::Result<Self> {
         let path = path.as_ref();
         let mut library = Library::load(path)?;
         let mtime = mtime(path)?;
         let client = load_symbol(&mut library)?;
-        let mut state = vec![0u8; client.state_size as usize];
-        (client.new_state)(state.as_mut_ptr(), (&mut api as *mut API).cast());
+
         Ok(Self{
             path: path.to_path_buf(),
             library,
             mtime,
             client,
-            state,
+            state: None,
             api,
         })
+    }
+
+    /// Updates the client and returns the status returned by the client's
+    /// update method.
+    fn update(&mut self) -> io::Result<Loop> {
+        let api = (&mut self.api as *mut API).cast();
+        if self.state.is_none() {
+            // We need to create the state
+            let mut state = vec![0u8; self.client.state_size];
+            (self.client.new_state)(state.as_mut_ptr(), api);
+            self.state = Some(state);
+            let cur_state = self.state.as_mut().unwrap();
+            // Call initialize
+            (self.client.initialize)(cur_state.as_mut_ptr(), api);
+            // Call reload
+            (self.client.reload)(cur_state.as_mut_ptr(), api);
+        }
+        // Here we must have a valid state
+        let cur_state = self.state.as_mut().unwrap();
+        // Check if we need to reload the library
+        let mtime = mtime(&self.path)?;
+        if mtime != self.mtime {
+            // We need to reload the library
+            // Call unload
+            (self.client.unload)(cur_state.as_mut_ptr(), api);
+            // Reload library and symbol
+            let mut library = Library::load(&self.path)?;
+            let client = load_symbol(&mut library)?;
+            self.library = library;
+            self.client = client;
+            self.mtime = mtime;
+            // Migrate the state
+            let mut new_state = vec![0u8; self.client.state_size];
+            (self.client.migrate_state)(
+                cur_state.as_mut_ptr(),
+                cur_state.len(),
+                new_state.as_mut_ptr(),
+                api);
+            self.state = Some(new_state);
+            let cur_state = self.state.as_mut().unwrap();
+            // Call reload
+            (self.client.reload)(cur_state.as_mut_ptr(), api);
+        }
+        let cur_state = self.state.as_mut().unwrap();
+        // Now perform the update
+        let loop_res = (self.client.update)(cur_state.as_mut_ptr(), api);
+        let loop_res = Loop::from_u32(loop_res);
+        if loop_res == Loop::Stop {
+            // Call unload
+            (self.client.unload)(cur_state.as_mut_ptr(), api);
+            // Call terminate
+            (self.client.terminate)(cur_state.as_mut_ptr(), api);
+            // Destroy the state
+            (self.client.drop_state)(cur_state.as_mut_ptr());
+            self.state = None;
+        }
+        Ok(loop_res)
     }
 }
 
@@ -85,6 +143,16 @@ pub enum Loop {
     Stop,
 }
 
+impl Loop {
+    fn from_u32(n: u32) -> Self {
+        if n == 0 { Self::Continue } else { Self::Stop }
+    }
+
+    fn to_u32(self) -> u32 {
+        if self == Self::Continue { 0 } else { 1 }
+    }
+}
+
 // ////////////////////////////////////////////////////////////////////////// //
 //                               Implementation                               //
 // ////////////////////////////////////////////////////////////////////////// //
@@ -102,7 +170,7 @@ fn load_symbol(library: &mut Library) -> io::Result<ClientApi> {
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct ClientApi {
-    state_size   : u64,
+    state_size   : usize,
     new_state    : fn(*mut u8, *mut u8),
     migrate_state: fn(*mut u8, usize, *mut u8, *mut u8),
     drop_state   : fn(*mut u8),
