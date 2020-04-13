9e0b43ba9e477ad740af8326301f9c540faceb4a
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-04-13 21:49:01 +0200

    Added docs for dynamic libraries

--- cacti_asset/src/dyn_lib.rs
+++ cacti_asset/src/dyn_lib.rs
@@ -2,7 +2,7 @@
 //!
 //! # Usage
 //!
-//! First you need to load a dynamic library:
+//! First you need to load a dynamic [Library](struct.Library.html):
 //!
 //! ```no_run
 //! # fn main() -> std::io::Result<()> {
@@ -13,23 +13,76 @@
 //! # }
 //! ```
 //!
-//! After that you can load symbols from it:
+//! After that you can load [Symbol](struct.Symbol.html)s from it:
 //!
-//! ```
+//! ```no_run
 //! # fn main() -> std::io::Result<()> {
 //! # use cacti_asset::dyn_lib::*;
 //! # let mut lib = Library::load("foo.dll")?;
-//! let sym: Symbol<extern "system" fn(u32) -> u32> = lib.load_symbol("GetProcessVersion");
+//! let sym: Symbol<extern "system" fn(u32) -> u32> = lib.load_symbol("times_two")?;
 //! // Calling it
 //! let raw_fn = *sym;
-//! let version = raw_fn(0);
+//! let four = raw_fn(2);
 //! # Ok(())
 //! # }
 //! ```
 //!
+//! The `Symbol` type's lifetime is tied to the `Library`'s, but de-referencing
+//! the symbol gets rid of the wrapper.
+//!
 //! # Porting the library to other platforms
 //!
-//! TODO
+//! To port this library to other platforms, the `trait DynLib` has to be
+//! implemented for a type and have it aliased as `DynLibImpl` in global scope
+//! for the platform:
+//!
+//! ```no_run
+//! # use std::io::Result;
+//! # use std::path::Path;
+//! # trait DynLib: Sized {
+//! #     type Symbol: std::fmt::Debug;
+//! #     fn load(path: &Path) -> Result<Self>;
+//! #     fn unload(&mut self);
+//! #     fn load_symbol(&mut self, name: &str) -> Result<Self::Symbol>;
+//! # }
+//! #[cfg(target_os = "new_platform")]
+//! mod my_platform {
+//!     /// Library type must be debug.
+//!     #[derive(Debug)]
+//!     struct MyPlatformDynLib { /* ... */ }
+//!
+//!     /// Symbol type must be debug and copy.
+//!     #[derive(Debug, Clone, Copy)]
+//!     struct MyPlatformSymbol { /* ... */ }
+//!
+//!     impl DynLib for MyPlatformDynLib {
+//!         /// The symbol type we defined for this platform.
+//!         type Symbol = MyPlatformSymbol;
+//!
+//!         /// Here you must load the library from the given path. Sometimes
+//!         /// there can be platform-specific behavior - like loading system
+//!         /// libraries without full path on Windows - that should be
+//!         /// documented.
+//!         fn load(path: &Path) -> Result<Self> {
+//!             // ...
+//! # unimplemented!()
+//!         }
+//!
+//!         /// Here you should unload the library, freeing up the resource.
+//!         fn unload(&mut self) {
+//!             // ...
+//!         }
+//!
+//!         /// Here you should load the symbol with the given name.
+//!         fn load_symbol(&mut self, name: &str) -> Result<Self::Symbol> {
+//!             // ...
+//! # unimplemented!()
+//!         }
+//!     }
+//! }
+//!
+//! #[cfg(target_os = "new_platform")] type DynLibImpl = my_platform::MyPlatformDynLib;
+//! ```
 
 // TODO: Doc platform-specific usage
 
@@ -42,15 +95,67 @@ use std::ops::Deref;
 //                                    API                                     //
 // ////////////////////////////////////////////////////////////////////////// //
 
-// TODO: doc
-
+/// Represents a loaded dynamic/shared library that gets unloaded when dropped.
+#[derive(Debug)]
 pub struct Library(DynLibImpl);
 
 impl Library {
+    /// Loads the dynamic/shared library from the given path.
+    ///
+    /// # Examples
+    ///
+    /// Let's load `Kernel32` on Windows:
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> {
+    /// use cacti_asset::dyn_lib::Library;
+    ///
+    /// let lib = Library::load("kernel32")?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// Loading a `dll` from the current working directory is not different:
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> {
+    /// use cacti_asset::dyn_lib::Library;
+    ///
+    /// let lib = Library::load("some_lib.dll")?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// In case of an IO or system error, an error variant is returned.
     pub fn load(path: impl AsRef<Path>) -> Result<Self> {
         Ok(Self(DynLibImpl::load(path.as_ref())?))
     }
 
+    /// Loads a symbol from this library with the given symbolic name.
+    ///
+    /// # Examples
+    ///
+    /// Loading and running `GetProcessVersion` from `Kernel32` on Windows:
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> {
+    /// use cacti_asset::dyn_lib::*;
+    ///
+    /// let mut lib = Library::load("kernel32")?;
+    /// let sym: Symbol<extern "system" fn(u32) -> u32> = lib.load_symbol("GetProcessVersion")?;
+    /// // Deref to get the function pointer itself
+    /// let get_ver = *sym;
+    /// // Call it
+    /// let ver = get_ver(0);
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// In case of an IO or system error, an error variant is returned.
     pub fn load_symbol<T>(&mut self, name: &str) -> Result<Symbol<T>> {
         Ok(Symbol{
             sym: self.0.load_symbol(name)?,
@@ -65,6 +170,27 @@ impl Drop for Library {
     }
 }
 
+/// Represents a loaded symbol. This symbol's lifetime is tied to the library
+/// it's loaded from to pervent usage after unloading the library.
+///
+/// The symbol can be de-reference to get the actual symbol value.
+///
+/// # Examples
+///
+/// Loading the `i32` symbol called `MAGIC_NUM` from the library `foo.so`:
+///
+/// ```no_run
+/// # fn main() -> std::io::Result<()> {
+/// use cacti_asset::dyn_lib::*;
+///
+/// let mut lib = Library::load("foo.so")?;
+/// let sym: Symbol<i32> = lib.load_symbol("MAGIC_NUM")?;
+/// // Getting the actual value
+/// let magic_num = *sym;
+/// # Ok(())
+/// # }
+/// ```
+#[derive(Debug, Clone, Copy)]
 pub struct Symbol<'a, T: 'a> {
     sym: <DynLibImpl as DynLib>::Symbol,
     phantom: PhantomData<&'a T>,
@@ -156,7 +282,7 @@ mod win32 {
     #[derive(Debug)]
     pub struct WinApiDynLib(*mut c_void);
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone, Copy)]
     pub struct WinApiSymbol(*const c_void);
 
     impl DynLib for WinApiDynLib {
@@ -218,7 +344,7 @@ mod unix {
     #[derive(Debug)]
     pub struct UnixDynLib(*mut c_void);
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone, Copy)]
     pub struct UnixSymbol(*const c_void);
 
     fn get_dlerror() -> io::Error {
@@ -275,6 +401,8 @@ mod unix {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // TODO: Cross-platform tests
 
     #[test]
     #[cfg(target_os = "windows")]
